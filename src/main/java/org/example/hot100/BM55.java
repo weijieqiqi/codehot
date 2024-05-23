cde8addaa683bf02cad6a7a0c264a1f7d0b4dc98
jiewei6 <jiewei6@iflytek.com>
2024-05-23 15:35:06 +0800

    5.22练习

--- src/main/java/org/example/hot100/BM55.java
+++ src/main/java/org/example/hot100/BM55.java
@@ -1,6 +1,8 @@
 package org.example.hot100;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * @Author: WeiJ
@@ -33,5 +35,46 @@ public class BM55 {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+
+
+
+    HashSet<String> set = new HashSet<>();
+    ArrayList<String> ans2 = new ArrayList<>();
+    public ArrayList<String> Permutation (String str) {
+        String[] chars = new String[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            chars[i] = String.valueOf(str.charAt(i));
+        }
+
+        process(chars, 0);
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            ans2.add(iterator.next());
+        }
+        return ans2;
+    }
+
+    public void process(String [] num, int start) {
+        if (start == num.length - 1) {
+            StringBuilder ans = new StringBuilder();
+            for (int i = 0; i < num.length; i++) {
+                ans.append(num[i]);
+            }
+            set.add(new String(ans));
+        }
+        for (int i = start; i < num.length; i++) {
+            swap(num, i, start);
+            process(num, start + 1);
+            swap(num, i, start);
+        }
+    }
+    public void swap(String[] num, int i, int j) {
+        String temp;
+        temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
     }
 }
